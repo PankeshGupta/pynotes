6266276f1abec04e9490ed8e74389af3c7cac182
Pankesh <post4pankesh@gmail.com>
2017-07-07 02:23:33 +0530

    introduction to object class

---
+++ object_class.py
@@ -0,0 +1,7 @@
+class abc(object):
+    pass
+# sso basically what we are to do here is that we are trying to create a new class with
+# the syntax using (object)
+# it means that we are inheriting from object class
+# it provides all the basic functionalities to the class such as the double
+# underscore methods
