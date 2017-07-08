bbcb50802461d3adcfc22efd88d0a09bc36d972f
Pankesh <post4pankesh@gmail.com>
2017-07-08 16:12:16 +0530

    adding notes

---
+++ another_extenion.py
@@ -0,0 +1,9 @@
+class LongNameDict(dict):
+    def longest_key(self):
+        longest = None
+        for key in self:
+            if not longest or len(key) > len(longest):
+                longest = key
+        return longest
+if __name__=="main":
+    main()
---
+++ python_tuple.py
@@ -0,0 +1,17 @@
+atuple = ([1,2,3,'list'],"hello",123)
+# here we are making a dummy tuple
+# which itself contains a list
+
+# we want to update the tuple by assignmet operator
+# it should throw us an error
+# which will be an type error which states the the type tuple doesnt support item assisgnment
+
+# but if we are storing a variable or a list or a object in the tuple
+# we can easily update the variable to the value which iit reffers
+# or we can easily update the object
+# or the list
+# we caanot append anything in the tuple as well
+
+a[0].append("new additon in list")
+
+# it works
