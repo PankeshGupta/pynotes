1abde9c509caa264631ce81ce2636701d9e65c94
Pankesh <post4pankesh@gmail.com>
2017-07-09 22:49:52 +0530

    application of mixins

---
+++ storing_adress.py
@@ -0,0 +1,15 @@
+class adress():
+    def __init__(self,country):
+        self.country = country
+    def route(self):
+        print("directing to ur location.......")
+class friend():
+    def __init__(self,name,adress):
+        self.name = name
+        self.adress = adress
+
+if __name__=="main":
+    main()
+    
+              
+        
