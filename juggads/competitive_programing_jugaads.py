91c83356b14fe4a84d69e190c3a34c20095e1651
Pankesh <post4pankesh@gmail.com>
2017-07-11 01:35:34 +0530

    some competitve prog. notes

---
+++ juggads/competitive_programing_jugaads.py
@@ -0,0 +1,14 @@
+# competitive programing jugaads
+
+#jugaad_1 convert string into integers or multiple assignmment
+
+x,y = [int(i) for i in input("please enter anything").split()]
+
+# jugaad_2 initialize x,y,z
+
+x = [i for i in range(n)]
+y = [i for i in range(n)]
+z = [i for i in range(n)]
+
+for i in range(n):
+    x[i],y[i],z[i] = [int(i) for i in input(" please enter Days , Lectures, Sadness ").split() ]
