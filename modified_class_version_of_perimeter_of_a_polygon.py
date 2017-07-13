c91d117cb460fb0e15679d59853bd0796467c4c5
Pankesh <post4pankesh@gmail.com>
2017-07-13 10:04:32 +0530

    advance_polygon_class

--- modified_class_version_of_perimeter_of_a_polygon.py
+++ modified_class_version_of_perimeter_of_a_polygon.py
@@ -1,17 +1,20 @@
 import math
-class point():
+class Point():
     def __init__(self,x,y):
         self.x = x
         self.y = y
     def distance(p1,p2):
         return math.sqrt((p1.x-p2.x)**2 +(p1.y-p2.y)**2)
-class polygon():
+class Polygon():
     # this doesnt do anything just initializes a vertices instance attribute
     def __init__(self,points = []):
         self.vertices = []
+        # we add this for loop to extend our polygon class
+        # using this we ca instantiate points by pasing them here
         for point in points:
             if isinstance(point,tuple):
-                self.vertices.append(Point(*point))
+                point = Point(*point)
+                self.vertices.append(point)
             self.vertices.append(point)    
         
     # call add_point : add_point(Point(1,1))
