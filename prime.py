0e21ea1141dd65bdd3ba0dd39412db95c8b1fce0
Pankesh <post4pankesh@gmail.com>
2017-07-24 21:07:48 +0530

    adding prime numbers

---
+++ prime.py
@@ -0,0 +1,10 @@
+n = input("pleaswe enter the number")
+for num in range(0,num+1):
+    if num>1:
+        for i in range(2,num):
+            if num%i==0:
+                pass
+            else:
+                print(num)
+    else:
+        pasa
